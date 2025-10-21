7b39472d40324a04d13962b37fbe985c6a4619b2
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-10-21 11:46:15 +0700

    Add files via upload

---
+++ js/command.js
@@ -0,0 +1,187 @@
+const fs = require("fs")
+const util = require("util")
+const moment = require("moment-timezone")
+const { exec } = require("child_process")
+const { red, green, yellow, blue, magenta, cyan, white } = require("kleur")
+
+const { getAdmin, parseMention } = require("../js/function")
+
+module.exports = async (sock, m, msg, chatUpdate, store) => {
+    try {
+        console.log(JSON.stringify(msg, null, 2))
+        const body = (m.mtype === "conversation" 
+            ? m.message.conversation : m.mtype === "imageMessage" 
+            ? m.message.imageMessage.caption : m.mtype === "videoMessage" 
+            ? m.message.videoMessage.caption : m.mtype === "extendedTextMessage" 
+            ? m.message.extendedTextMessage.text : m.mtype === "buttonsResponseMessage" 
+            ? m.message.buttonsResponseMessage.selectedButtonId : m.mtype === "listResponseMessage" 
+            ? m.message.listResponseMessage.singleSelectReply.selectedRowId : m.mtype === "interactiveMessage" 
+            ? m.message.interactiveMessage?.buttonReply?.buttonId : "")
+        const budy = (typeof m.text == "string" ? m.text : "")
+        
+        const prefix = "!"
+        const ctrCmd = body || ""
+        const command = ctrCmd.toLowerCase().split(" ")[0] || ""
+        const isCmd = command.startsWith(prefix)
+        
+        const args = body.trim().split(/ +/).slice(1)
+        const text = q = args.join(" ")
+        const sender = m.key.fromMe ? sock.user.id.split(":")[0] + "@s.whatsapp.net" || sock.user.id : m.key.participant || m.key.remoteJid
+        const date = moment.tz("Asia/Jakarta").format("DD/MM/YYYY")
+        const time = moment.tz("Asia/Jakarta").format("HH:mm:ss")
+        const botNumber = await sock.decodeJid(sock.user.id)
+        const groupMetadata = m.isGroup ? await sock.groupMetadata(m.chat) : ""
+        const participants = m.isGroup ? await groupMetadata.participants : ""
+        const groupAdmins = m.isGroup ? await getAdmin(participants) : ""
+        const isGroupAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+        const isBotGroupAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
+    
+        // LOG
+        if (isCmd && !m.isGroup) {
+            // console.log("[CMD] " + time + " from " + sender + " in Private chat.")
+            console.log(
+              green("[CMD]") +
+                " " +
+                yellow(time) +
+                " " +
+                green("from") +
+                " " +
+                yellow(sender) +
+                " " +
+                green("in") +
+                " " +
+                white("Private chat.")
+            );
+        }
+        if (isCmd && m.isGroup) {
+            // console.log("[CMD] " + time + " from " + sender + " in Group chat.")
+            console.log(
+              green("[CMD]") +
+                " " +
+                yellow(time) +
+                " " +
+                green("from") +
+                " " +
+                yellow(sender) +
+                " " +
+                green("in") +
+                " " +
+                white("Group chat.")
+            );
+        }
+        
+        switch (command) {
+            case prefix+"help": case prefix+"menu": {
+                const description = `👋 Hallo!
+
+Bot ini dibuat bagian dari hiburan belaka.
+https://mysu-inky.vercel.app
+
+⏲️ ${time} WiB
+
+┌ ${prefix}help
+├ ${prefix}close 
+├ ${prefix}revoke
+├ ${prefix}grouplink
+└ ${prefix}null
+
+Dikembangkan oleh @6281410404318`
+                sock.sendMessage(m.chat, { text: description, mentions: parseMention(description) }, { quoted: m })
+                break
+            }
+            // BAGIAN ADMIN
+            case prefix+"close": {
+                if (!m.isGroup) return
+                if (!isBotGroupAdmins) return m.reply("Perintah ini hanya bisa di gunakan ketika bot menjadi admin!")
+                if (!isGroupAdmins) return m.reply("Perintah ini hanya bisa di gunakan oleh admin!")
+                if (args[0] === "false") {
+                sock.groupSettingUpdate(m.chat, "not_announcement")
+                } else if (args[0] === "true") {
+                await sock.groupSettingUpdate(m.chat,"announcement")
+                } else {
+                    const button = [{ buttonId: "!close true", buttonText: { displayText: "true" }}, { buttonId: "!close false", buttonText: { displayText: "false" }}]
+                    await sock.sendMessage(m.chat, { text: "Untuk menutup group tekan *true* dan membuka group tekan *false*", footer: "https://mysu-inky.vercel.app", buttons: button })
+                // await sock.groupSettingUpdate(m.chat, "announcement")
+                }
+                break
+            }
+            case prefix+"revoke": {
+                if (!m.isGroup) return
+                if (!isBotGroupAdmins) return m.reply("Perintah ini hanya bisa di gunakan ketika bot menjadi admin!")
+                if (!isGroupAdmins) return m.reply("Perintah ini hanya bisa di gunakan oleh admin!")
+                await sock.groupRevokeInvite(m.chat)
+                break
+            }
+            case prefix+"grouplink": {
+                if (!m.isGroup) return
+                if (!isBotGroupAdmins) return m.reply("Perintah ini hanya bisa di gunakan ketika bot menjadi admin!")
+                if (!isGroupAdmins) return m.reply("Perintah ini hanya bisa di gunakan oleh admin!")
+                const nlink = await sock.groupInviteCode(m.chat)
+                m.reply("https://chat.whatsapp.com/" + nlink)
+                break
+            }
+            default:
+            if (budy.startsWith("$")) {
+            // if (!m.key.fromMe && !isDev) return
+            const command = budy.slice(2) // Mengambil perintah setelah "$ "
+        
+            // Menjalankan perintah shell
+            exec(command, (err, stdout) => {
+                if (err) {
+                    return m.reply(`${err}`) // Kirim pesan error jika terjadi kesalahan
+                }
+                if (stdout) {
+                    return m.reply(stdout) // Kirim output jika perintah berhasil
+                }
+            })
+            }
+            if (budy.startsWith("..")) {
+            // if (!m.key.fromMe && !isDev) return
+            try {
+                // Mengambil pesan setelah "..""
+                const result = await eval(`(async () => { return ${budy.slice(3)} })()`)
+                
+                // Fungsi untuk memformat dan mengirim balasan
+                m.reply(util.format(result))
+            } catch (e) {
+                m.reply(String(e)) // Tangani error dan kirim pesan error
+            }
+            }
+            if (budy.startsWith("=>")) {
+            // if (!m.key.fromMe && !isDev) return
+            const konsol = budy.slice(3) // Mengambil pesan setelah "=>"
+        
+            // Fungsi untuk memformat dan mengirim balasan
+            const Return = (sul) => {
+                let sat = JSON.stringify(sul, null, 2) // Mengubah objek ke string JSON
+                let bang = util.format(sat)
+        
+                if (sat === undefined) {
+                    bang = util.format(sul) // Jika undefined, format langsung
+                }
+        
+                return m.reply(bang) // Kirim balasan
+            }
+        
+            try {
+                // Mengeksekusi kode yang diberikan
+                const result = eval(`(async () => { ${konsol} })()`)
+                m.reply(util.format(result)) // Kirim hasil eksekusi
+            } catch (e) {
+                m.reply(String(e)) // Tangani error dan kirim pesan error
+            }
+        }
+        }
+    } catch (error) {
+      console.error(error)
+      m.reply("💔 Server bermasalah.")
+  }
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log("Memperbarui js/command.js")
+  delete require.cache[file]
+  require(file)
+})
---
+++ js/function.js
@@ -0,0 +1,134 @@
+const {
+    proto,
+    getContentType,
+    areJidsSameUser,
+    generateWAMessage
+} = require("wa-bot")
+
+exports.getAdmin = (participants) => {
+    let admins = []
+    for (let i of participants) {
+        i.admin === "superadmin" ? admins.push(i.id) :  i.admin === "admin" ? admins.push(i.id) : ""
+    }
+    return admins || []
+}
+
+exports.parseMention = (text = '') => {
+    return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
+}
+
+exports.smsg = (sock, m, store) => {
+    if (!m) return m
+    let M = proto.WebMessageInfo
+    if (m.key) {
+        m.id = m.key.id
+        m.isBaileys = m.id.startsWith('BAE5') && m.id.length === 16
+        m.chat = m.key.remoteJid
+        m.fromMe = m.key.fromMe
+        m.isGroup = m.chat.endsWith('@g.us')
+        m.sender = sock.decodeJid(m.fromMe && sock.user.id || m.participant || m.key.participant || m.chat || '')
+        if (m.isGroup) m.participant = sock.decodeJid(m.key.participant) || ''
+    }
+    if (m.message) {
+        m.mtype = getContentType(m.message)
+        m.msg = (m.mtype == 'viewOnceMessage' ? m.message[m.mtype].message[getContentType(m.message[m.mtype].message)] : m.message[m.mtype])
+        m.body = m.message.conversation || m.msg.caption || m.msg.text || (m.mtype == 'listResponseMessage') && m.msg.singleSelectReply.selectedRowId || (m.mtype == 'buttonsResponseMessage') && m.msg.selectedButtonId || (m.mtype == 'viewOnceMessage') && m.msg.caption || m.text
+        let quoted = m.quoted = m.msg.contextInfo ? m.msg.contextInfo.quotedMessage : null
+        m.mentionedJid = m.msg.contextInfo ? m.msg.contextInfo.mentionedJid : []
+        if (m.quoted) {
+            let type = Object.keys(m.quoted)[0]
+			m.quoted = m.quoted[type]
+            if (['productMessage'].includes(type)) {
+				type = Object.keys(m.quoted)[0]
+				m.quoted = m.quoted[type]
+			}
+            if (typeof m.quoted === 'string') m.quoted = {
+				text: m.quoted
+			}
+            m.quoted.mtype = type
+            m.quoted.id = m.msg.contextInfo.stanzaId
+			m.quoted.chat = m.msg.contextInfo.remoteJid || m.chat
+            m.quoted.isBaileys = m.quoted.id ? m.quoted.id.startsWith('BAE5') && m.quoted.id.length === 16 : false
+			m.quoted.sender = sock.decodeJid(m.msg.contextInfo.participant)
+			m.quoted.fromMe = m.quoted.sender === sock.decodeJid(sock.user.id)
+            m.quoted.text = m.quoted.text || m.quoted.caption || m.quoted.conversation || m.quoted.contentText || m.quoted.selectedDisplayText || m.quoted.title || ''
+			m.quoted.mentionedJid = m.msg.contextInfo ? m.msg.contextInfo.mentionedJid : []
+            m.getQuotedObj = m.getQuotedMessage = async () => {
+			if (!m.quoted.id) return false
+			let q = await store.loadMessage(m.chat, m.quoted.id, sock)
+ 			return exports.smsg(sock, q, store)
+            }
+            let vM = m.quoted.fakeObj = M.fromObject({
+                key: {
+                    remoteJid: m.quoted.chat,
+                    fromMe: m.quoted.fromMe,
+                    id: m.quoted.id
+                },
+                message: quoted,
+                ...(m.isGroup ? { participant: m.quoted.sender } : {})
+            })
+
+            /**
+             * 
+             * @returns 
+             */
+            m.quoted.delete = () => sock.sendMessage(m.quoted.chat, { delete: vM.key })
+
+	   /**
+		* 
+		* @param {*} jid 
+		* @param {*} forceForward 
+		* @param {*} options 
+		* @returns 
+	   */
+            m.quoted.copyNForward = (jid, forceForward = false, options = {}) => sock.copyNForward(jid, vM, forceForward, options)
+
+            /**
+              *
+              * @returns
+            */
+            m.quoted.download = () => sock.downloadMediaMessage(m.quoted)
+        }
+    }
+    if (m.msg.url) m.download = () => sock.downloadMediaMessage(m.msg)
+    m.text = m.msg.text || m.msg.caption || m.message.conversation || m.msg.contentText || m.msg.selectedDisplayText || m.msg.title || ''
+    /**
+	* Reply to this message
+	* @param {String|Object} text 
+	* @param {String|false} chatId 
+	* @param {Object} options 
+	*/
+    m.reply = (text, chatId = m.chat, options = {}) => Buffer.isBuffer(text) ? sock.sendMedia(chatId, text, 'file', '', m, { ...options }) : sock.sendText(chatId, text, m, { ...options })
+    /**
+	* Copy this message
+	*/
+	m.copy = () => exports.smsg(sock, M.fromObject(M.toObject(m)))
+
+	/**
+	 * 
+	 * @param {*} jid 
+	 * @param {*} forceForward 
+	 * @param {*} options 
+	 * @returns 
+	 */
+	m.copyNForward = (jid = m.chat, forceForward = false, options = {}) => sock.copyNForward(jid, m, forceForward, options)
+	
+	sock.appenTextMessage = async(text, chatUpdate) => {
+        let messages = await generateWAMessage(m.chat, { text: text, mentions: m.mentionedJid }, {
+            userJid: sock.user.id,
+            quoted: m.quoted && m.quoted.fakeObj
+        })
+        messages.key.fromMe = areJidsSameUser(m.sender, sock.user.id)
+        messages.key.id = m.key.id
+        messages.pushName = m.pushName
+        if (m.isGroup) messages.participant = m.sender
+        let msg = {
+            ...chatUpdate,
+            messages: [proto.WebMessageInfo.fromObject(messages)],
+            type: 'append'
+        }
+        sock.ev.emit('messages.upsert', msg)
+    }
+
+    return m
+}
